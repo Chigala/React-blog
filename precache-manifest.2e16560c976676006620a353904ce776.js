a989a12a2fd48064d632648fa7f0c8a1eed3316d
Chigala Enyeazu <chigala11@gmail.com>
2022-03-16 12:21:10 +0100

    Updates

---
+++ precache-manifest.2e16560c976676006620a353904ce776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5acc3721fe5452de581",
+    "url": "/react-blog/static/css/main.905ad6f5.chunk.css"
+  },
+  {
+    "revision": "f5acc3721fe5452de581",
+    "url": "/react-blog/static/js/main.f5acc372.chunk.js"
+  },
+  {
+    "revision": "5c361c5ab59499c6f6f7",
+    "url": "/react-blog/static/js/1.5c361c5a.chunk.js"
+  },
+  {
+    "revision": "e82d67cde82345958961",
+    "url": "/react-blog/static/js/2.e82d67cd.chunk.js"
+  },
+  {
+    "revision": "51c30ce73f2037f5c2f8",
+    "url": "/react-blog/static/js/runtime~main.51c30ce7.js"
+  },
+  {
+    "revision": "03ee28b5620653b9ff3d8a9db8bb2373",
+    "url": "/react-blog/index.html"
+  }
+];
